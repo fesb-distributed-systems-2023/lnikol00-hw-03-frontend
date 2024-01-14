26588845a59f893dfe1f87fa082ccc334bdf0dca
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-15 00:54:30 +0100

    adding getAll and getSingle api calls

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,64 +1,69 @@
-import { useEffect, useState } from 'react'
-import { PlaneListState } from '../../interfaces/types'
+import { useEffect, useRef, useState } from 'react'
 import Pagination from './Pagination';
 import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../Redux/hooks';
-import { getAllPlanes } from '../../Redux/Actions/PlaneAction';
+import { PlaneList } from '../../interfaces/types';
+import axios, { AxiosError } from 'axios';
+import { baseURL } from '../../hooks/baseURL';
+
 
 function Home() {
 
-    // const [planes, setPlanes] = useState<PlaneListState>([])
+    const [planes, setPlanes] = useState<PlaneList>([])
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [planesPerPage, setPlanesPerPage] = useState<number>(5);
+
+    const [errMsg, setErrMsg] = useState('')
+    const errRef = useRef<null | HTMLParagraphElement>(null)
 
     const lastPlaneIndex = currentPage * planesPerPage;
     const firstPlaneIndex = lastPlaneIndex - planesPerPage;
 
-    // useEffect(() => {
-    //     fetch("http://localhost:8000/planes/")
-    //         .then((response) => {
-    //             return response.json()
-    //         })
-    //         .then((json) => {
-    //             setPlanes(json)
-    //         })
-    // }, [])
-
-    const dispatch = useAppDispatch()
-    const planeList = useAppSelector(state => state.planeList)
-
-    const { loading, error, planes } = planeList;
+    useEffect(() => {
+        setErrMsg('');
+    }, [planes])
 
     useEffect(() => {
-        dispatch(getAllPlanes());
-    }, [dispatch])
+        const getPlanes = async () => {
+            try {
+                const { data } = await axios.get(`${baseURL}/api/Airport`)
+                setPlanes(data);
+            } catch (error) {
+                const err = error as AxiosError
+                if (!err.response) {
+                    setErrMsg("No server response!");
+                }
+                if (errRef.current)
+                    errRef.current.focus();
+            }
+        }
+        getPlanes();
+    })
 
     return (
         <div className='flex flex-col justify-start md:my-[2rem] my-[1.5rem] md:mx-[10rem] mx-[3rem]'>
             <span className='md:text-2xl text-xl'>Currently available planes:</span>
-            {
-                loading ? (<div>Loading...</div>) : error ? (<div>Error</div>) :
-                    (
-                        <>
-                            {planes.slice(firstPlaneIndex, lastPlaneIndex).map((plane) => {
-                                return (
-                                    <Link to={`/planes/${plane.id}`}>
-                                        <div
-                                            key={plane.id}
-                                            className='flex flex-start justify-start flex-col bg-white py-[10px] px-[16px] md:my-[10px] my-[8px] border-[1px] border-black border-solid shadow-md cursor-pointer'
-                                        >
-                                            <div>
-                                                <h1>{plane.model}</h1>
-                                                <span>Country of origin: {plane.country}</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                )
-                            })}
-                        </>
-                    )
-            }
-
+            <p
+                ref={errRef}
+                className={errMsg ? 'bg-[lightpink] text-[firebrick] font-bold p-2 my-2' : 'absolute left-[-9999px]'}
+                aria-live="assertive"
+            >
+                {errMsg}
+            </p>
+            {planes.slice(firstPlaneIndex, lastPlaneIndex).map((plane) => {
+                return (
+                    <Link to={`/planes/${plane.id}`}>
+                        <div
+                            key={plane.id}
+                            className='flex flex-start justify-start flex-col bg-white py-[10px] px-[16px] md:my-[10px] my-[8px] border-[1px] border-black border-solid shadow-md cursor-pointer'
+                        >
+                            <div>
+                                <h1>{plane.model}</h1>
+                                <span>Country of origin: {plane.country}</span>
+                            </div>
+                        </div>
+                    </Link>
+                )
+            })}
             <Pagination
                 totalPlanes={planes.length}
                 planesPerPage={planesPerPage}
--- client/src/pages/home/PlaneDetails.tsx
+++ client/src/pages/home/PlaneDetails.tsx
@@ -1,26 +1,72 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import testImage from "../../utils/images/test.jpg"
+import { Plane } from '../../interfaces/types';
+import { baseURL } from '../../hooks/baseURL';
+import axios, { AxiosError } from 'axios';
 
 function PlaneDetails() {
 
+    const [plane, setPlane] = useState<Plane>()
+    const params = useParams();
+
+    const [errMsg, setErrMsg] = useState('')
+    const errRef = useRef<null | HTMLParagraphElement>(null)
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [plane])
+
+    useEffect(() => {
+        const getSinglePlane = async () => {
+            try {
+                const { data } = await axios.get(`${baseURL}/api/Airport/${params.id}`)
+                setPlane(data);
+            }
+            catch (error) {
+                const err = error as AxiosError
+                if (!err.response) {
+                    setErrMsg("No server response!")
+                }
+                else if (err.response?.status === 404) {
+                    setErrMsg("Plane not found!")
+                }
+                if (errRef.current)
+                    errRef.current.focus();
+            }
+        }
+        getSinglePlane();
+    }, [params])
+
     return (
         <div className='flex md:flex-row flex-col justify-start items-center md:p-[5rem] p-[1.5rem] '>
-            <div className='md:w-[50%] w-[100%]'>
-                <img
-                    src={testImage}
-                    alt='test'
-                    className='w-[100%] md:h-[450px] h-[200px]'
-                />
-            </div>
-            <div className='flex justify-center items-center flex-col md:w-[50%] w-[100%] md:px-[2em] md:py-[0em] py-[0.75em] gap-5'>
-                <span className='text-xl'><b>Model: </b>Plane Name</span>
-                <span className='text-xl'><b>Year: </b>1999</span>
-                <span className='text-xl'><b>Country: </b>Croatia</span>
-                <span className='text-xl'><b>Capacity: </b>200</span>
-                <span className='text-xl'><b>Routes: </b>200</span>
-                <span className='text-xl'><b>Crew: </b>200</span>
-            </div>
+            <p
+                ref={errRef}
+                className={errMsg ? 'bg-[lightpink] text-[firebrick] font-bold p-2 my-2 w-screen' : 'absolute left-[-9999px]'}
+                aria-live="assertive"
+            >
+                {errMsg}
+            </p>
+            {
+                plane &&
+                <>
+                    <div className='md:w-[50%] w-[100%]'>
+                        <img
+                            src={plane?.image}
+                            alt='test'
+                            className='w-[100%] md:h-[450px] h-[200px]'
+                        />
+                    </div>
+                    <div className='flex justify-center items-center flex-col md:w-[50%] w-[100%] md:px-[2em] md:py-[0em] py-[0.75em] gap-5'>
+                        <span className='text-xl'><b>Model: </b>{plane?.model}</span>
+                        <span className='text-xl'><b>Year: </b>{plane?.year}</span>
+                        <span className='text-xl'><b>Country: </b>{plane?.country}</span>
+                        <span className='text-xl'><b>Capacity: </b>{plane?.capacity}</span>
+                        <span className='text-xl'><b>Type: </b>{plane?.type}</span>
+                        <span className='text-xl'><b>Captain: </b>{plane?.captain}</span>
+                    </div>
+                </>
+            }
         </div >
     )
 }
