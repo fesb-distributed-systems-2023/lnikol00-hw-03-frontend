4b71b4893914a735a31dbdf2c4f1e8062798bf6c
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-16 09:48:39 +0100

    Adding update api call

---
+++ client/src/pages/create/Update.tsx
@@ -0,0 +1,175 @@
+import axios, { AxiosError } from 'axios'
+import React, { useEffect, useRef, useState } from 'react'
+import { baseURL } from '../../hooks/baseURL'
+import Toast from '../../utils/messages/Toast'
+import { toast } from "react-toastify"
+import { Plane } from '../../interfaces/types'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function Update() {
+
+    const [model, setModel] = useState<string>("")
+    const [year, setYear] = useState<number>(0)
+    const [image, setImage] = useState<string>("")
+    const [country, setCountry] = useState<string>("")
+    const [type, setType] = useState<string>("")
+    const [capacity, setCapacity] = useState<number>(0)
+    const [captain, setCaptain] = useState<string>("")
+    const [plane, setPlane] = useState<Plane>()
+
+    const navigate = useNavigate()
+    const params = useParams()
+
+    useEffect(() => {
+        const getSinglePlane = async () => {
+            try {
+                const { data } = await axios.get(`${baseURL}/api/Airport/${params.id}`)
+                setPlane(data);
+            }
+            catch (error) {
+                const err = error as AxiosError
+                if (!err.response) {
+                    throw new Error("No server response!")
+                }
+                else if (err.response?.status === 404) {
+                    throw new Error(`${err.response.data}`)
+                }
+            }
+        }
+        getSinglePlane();
+    }, [params])
+
+    useEffect(() => {
+        setModel(`${plane?.model}`)
+        setImage(`${plane?.image}`)
+        setCaptain(`${plane?.captain}`)
+        setType(`${plane?.type}`)
+        setCountry(`${plane?.country}`)
+        setYear(Number(plane?.year))
+        setCapacity(Number(plane?.capacity))
+    }, [plane?.model, plane?.image, plane?.capacity, plane?.captain, plane?.country, plane?.type, plane?.year])
+
+    const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const updatePlane = {
+                model: model,
+                year: year,
+                image: image,
+                country: country,
+                capacity: capacity,
+                type: type,
+                captain: captain
+            }
+
+            const editPlane = await axios.put(
+                `${baseURL}/api/Airport/${params.id}`,
+                updatePlane, {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+            })
+            if (editPlane) {
+                toast.success("Plane Updated!")
+                console.log(editPlane.data);
+            }
+        } catch (error) {
+            const err = error as AxiosError
+            if (!err.response) {
+                toast.error("No server response!")
+            }
+            else if (err.response?.status === 400) {
+                toast.error(`${err.response.data}`)
+            }
+        }
+    }
+
+    return (
+        <div className='flex flex-col justify-center items-center md:pt-5 pt-3'>
+            <Toast />
+            <h2 className='md:text-3xl text-xl md:pb-5 pb-2'>Update plane</h2>
+            <form
+                onSubmit={handleEdit}
+                className='flex flex-col justify-center items-center md:gap-[20px] gap-[15px]'
+            >
+                <label className='relative' id='model'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.model}
+                        onChange={(e) => setModel(e.target.value)}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Model</span>
+                </label>
+                <label className='relative' id='image'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.image}
+                        onChange={(e) => setImage(e.target.value)}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Image</span>
+                </label>
+                <label className='relative' id='year'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.year}
+                        onChange={(e) => setYear(Number(e.target.value))}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Year</span>
+                </label>
+                <label className='relative' id='country'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.country}
+                        onChange={(e) => setCountry(e.target.value)}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Country</span>
+                </label>
+                <label className='relative' id='capacity'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.capacity}
+                        onChange={(e) => setCapacity(Number(e.target.value))}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Capacity</span>
+                </label>
+                <label className='relative' id='type'>
+                    <select
+                        required
+                        defaultValue={plane?.type}
+                        onChange={(e) => setType(e.target.value)}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none transition duration-200'
+                    >
+                        <option value="Cargo">Cargo</option>
+                        <option value="Passenger">Passenger</option>
+                        <option value="Private">Private</option>
+                    </select>
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Type</span>
+                </label>
+                <label className='relative' id='captain'>
+                    <input
+                        type='text'
+                        required
+                        defaultValue={plane?.captain}
+                        onChange={(e) => setCaptain(e.target.value)}
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Captain</span>
+                </label>
+                <button className='bg-white w-[150px] h-[45px] uppercase rounded-lg outline-none'>Update</button>
+            </form>
+        </div>
+    )
+}
+
+export default Update
