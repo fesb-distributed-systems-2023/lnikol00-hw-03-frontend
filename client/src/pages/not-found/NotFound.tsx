581de29d6f6d2ed124b47a70cdc6fe57cac2027f
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-16 15:25:02 +0100

    Adding 404 page

--- client/src/pages/not-found/NotFound.tsx
+++ client/src/pages/not-found/NotFound.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
     return (
-        <div>
-
+        <div className='text-center m-[50px]'>
+            <h2 className='text-[6rem]'>404</h2>
+            <p className='py-[20px] text-[35px]'>Page not found</p>
+            <span className='text-[18px]'>The page that you are looking for doesnt exist or some other error occured.</span>
+            <p className='py-[10x] text-[18px]'><Link to="/">Return back to the home page...</Link></p>
         </div>
     )
 }
